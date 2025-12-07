21c871db9e59fb249044253c38dd5dd86ae4406d
Tebatso <tebatsomangena8@gmail.com>
2025-12-07 07:30:19 +0200

    inngest configured

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,9 +2,11 @@ import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
 
 import { ENV } from "./config/env.js";
 import { connectDB } from "./config/db.js";
+import { inngest, functions } from "./config/inngest.js";
 
 const app = express();
 
@@ -14,12 +16,14 @@ const { PORT } = ENV;
 app.use(express.json());
 app.use(cors());
 app.use(morgan("dev"));
-app.use(clerkMiddleware())
+app.use(clerkMiddleware());
 
 // routes
 app.get("/", (req,res) => {
   res.status(200).send("READY TO WORK")
 })
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 // connect DB
 connectDB().then(() => {
