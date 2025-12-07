8d567cfdee84a24711c05dc379d104d10fd4dcc2
Tebatso <tebatsomangena8@gmail.com>
2025-12-07 07:08:44 +0200

    updated the env file

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,5 +2,9 @@ import "dotenv/config";
 
 export const ENV = {
   PORT: process.env.PORT || 5050,
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
+  CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
+  CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+  INNGEST_SIGNING_KEY: process.env.INNGEST_SIGNING_KEY
 }
